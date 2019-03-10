6380ab4077c0b1b24ce9f0b543c24602d034ca86
yeahDDD <305682379@qq.com>
2019-03-10 15:35:43 +0800

    auto deploy

---
+++ precache-manifest.1291cc9b7eecbcff698cae0eeac6710c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3af067ca4f0ebc0cbb20c78dace5e08",
+    "url": "https://rosong1.github.io/foodPage/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "2bb64a38b53c1be98d45",
+    "url": "https://rosong1.github.io/foodPage/umi.css"
+  },
+  {
+    "revision": "2bb64a38b53c1be98d45",
+    "url": "https://rosong1.github.io/foodPage/umi.js"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "https://rosong1.github.io/foodPage/pwacompat.min.js"
+  },
+  {
+    "revision": "9d7f1b81f0bab568ef6336be4b0da809",
+    "url": "https://rosong1.github.io/foodPage/manifest.json"
+  },
+  {
+    "revision": "8fb89a27b1dd38f26cf73be06636fca2",
+    "url": "https://rosong1.github.io/foodPage/index.html"
+  },
+  {
+    "revision": "a957fcfa082b99401fd286f6f407f50a",
+    "url": "https://rosong1.github.io/foodPage/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "1bb884afb41b7047e2785622311c8182",
+    "url": "https://rosong1.github.io/foodPage/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "5f62e89b6733c74063f60083b279a29f",
+    "url": "https://rosong1.github.io/foodPage/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "1b296332679f81ee413c86e6af1e84d9",
+    "url": "https://rosong1.github.io/foodPage/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "c504ad31c56a9f458b53d336bda8fd0a",
+    "url": "https://rosong1.github.io/foodPage/icons/android-chrome-512x512.png"
+  }
+];
