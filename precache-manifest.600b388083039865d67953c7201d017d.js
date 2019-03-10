76eedddbf6e9c99971b4a45b9cc4f5aa7dacf981
yeahDDD <305682379@qq.com>
2019-03-10 15:07:03 +0800

    auto deploy

---
+++ precache-manifest.600b388083039865d67953c7201d017d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bb64a38b53c1be98d45",
+    "url": "https://rosong1.github.io/foodPage/umi.js"
+  },
+  {
+    "revision": "2bb64a38b53c1be98d45",
+    "url": "https://rosong1.github.io/foodPage/umi.css"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "https://rosong1.github.io/foodPage/pwacompat.min.js"
+  },
+  {
+    "revision": "a2e6f2cba31febe7d17c130f0686bb78",
+    "url": "https://rosong1.github.io/foodPage/manifest.json"
+  },
+  {
+    "revision": "e97c3418d6bbd55324ed4e0c02b1e434",
+    "url": "https://rosong1.github.io/foodPage/index.html"
+  }
+];
