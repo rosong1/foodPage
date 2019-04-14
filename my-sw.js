ee2924ed26755ce67c897cd6edf552682de95f01
yeahDDD <305682379@qq.com>
2019-04-14 17:43:06 +0800

    auto deploy

--- my-sw.js
+++ my-sw.js
@@ -1,4 +1,4 @@
-importScripts("https://rosong1.github.io/foodPage/precache-manifest.de8b136296632edbe56e547c614ab39c.js", "https://rosong1.github.io/foodPage/workbox-v3.6.3/workbox-sw.js");
+importScripts("https://rosong1.github.io/foodPage/precache-manifest.6585884d9d2c990b10560e017bbd3900.js", "https://rosong1.github.io/foodPage/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "https://rosong1.github.io/foodPage/workbox-v3.6.3"});
 /* globals workbox */
 /* eslint-disable no-restricted-globals */
